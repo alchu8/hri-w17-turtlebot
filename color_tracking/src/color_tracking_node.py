d7d987bcf74246556922a46aeb5cd1ec68dfb419
turtlebot <rinsurbs@gmail.com>
2017-02-20 21:41:33 -0500

    color tracking node

--- color_tracking/src/color_tracking_node.py
+++ color_tracking/src/color_tracking_node.py
@@ -16,7 +16,7 @@ def callback(data):
         for obj in data.blobs:
             blob_position = blob_position + obj.x
             blob_position = blob_position/len(data.blobs)
-            rospy.loginfo("blob position:"+blob_position)
+            rospy.loginfo(blob_position)
 
 def run():
     #global blob_position
@@ -24,8 +24,9 @@ def run():
     #pub = rospy.Publisher('/mobile_base/commands/velocity', Twist)
 
     #subscribe to the robot sensor state
-    rospy.Subscriber('/blobs', Blobs, callback)
     rospy.init_node("color_tracker")
+    rospy.Subscriber('/blobs', Blobs, callback)
+    rospy.spin()
 
 if __name__ == '__main__':
     try:
