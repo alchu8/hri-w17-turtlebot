bb7cfd6454a2164d7593fa058db75bfd71db9c10
alchu8 <alchu8@gmail.com>
2017-03-12 20:20:35 -0700

    sync with depth

--- face_evaluator/src/face_evaluator_node.cpp
+++ face_evaluator/src/face_evaluator_node.cpp
@@ -4,6 +4,11 @@
 #include <std_msgs/Int8.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <sensor_msgs/Image.h>
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -21,18 +26,25 @@
 
 using namespace dlib;
 using namespace std;
+using namespace sensor_msgs;
+using namespace message_filters;
 
 #define REF_SIZE 10
 
 //static const std::string OPENCV_WINDOW = "Image window";
 shape_predictor sp_; // give shape_predictor_68_face_landmarks.dat in command line
+typedef sync_policies::ApproximateTime<Image, Image> MySyncPolicy;
+typedef message_filters::Subscriber<sensor_msgs::Image> image_sub_type;
 
 class ImageConverter
 {
   ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
+  //image_transport::ImageTransport it_;
+  //image_transport::Subscriber image_sub_;
+  //image_transport::Publisher image_pub_;
+  message_filters::Subscriber<Image> *rgb_sub_;
+  message_filters::Subscriber<Image> *depth_sub_;
+  message_filters::Synchronizer<MySyncPolicy>* my_sync_;
   ros::Publisher expression_pub_;
   frontal_face_detector detector_;
   image_window win_;
@@ -41,14 +53,20 @@ class ImageConverter
   
 public:
   ImageConverter()
-    : it_(nh_)//, templates_(REF_SIZE)
+    //: it_(nh_), templates_(REF_SIZE)
   {
     detector_ = get_frontal_face_detector();
     //countTime = 0;
-    // Subscrive to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/camera/rgb/image_raw", 1, 
+    // Subscribe to input video feed and publish output video feed
+    int queueSize = 10;
+    rgb_sub_ = new image_sub_type(nh_, "/camera/rgb/image_raw", queueSize);
+    depth_sub_ = new image_sub_type(nh_, "/camera/depth/image_raw", queueSize);
+    my_sync_ = new message_filters::Synchronizer<MySyncPolicy>(MySyncPolicy(queueSize),  *rgb_sub_, *depth_sub_);
+    my_sync_->registerCallback(boost::bind(&ImageConverter::imageCb, this, _1, _2));
+
+    /*image_sub_ = it_.subscribe("/camera/rgb/image_raw", 1, 
       &ImageConverter::imageCb, this);
-    image_pub_ = it_.advertise("/image_converter/output_video", 1);
+    image_pub_ = it_.advertise("/image_converter/output_video", 1);*/
     expression_pub_ = nh_.advertise<std_msgs::Int8>("/face_evaluator/expression", 1);
 
     //cv::namedWindow(OPENCV_WINDOW);
@@ -57,6 +75,9 @@ public:
   ~ImageConverter()
   {
     //cv::destroyWindow(OPENCV_WINDOW);
+    delete my_sync_;
+    delete rgb_sub_;
+    delete depth_sub_;
   }
 
   void initTemplates(char* templatesPath)
@@ -75,7 +96,6 @@ public:
           continue;
         }
         unsigned long index = ent->d_name[0] - '0'; // index of reference image
-        //load_jpeg(face_chips[index], ent->d_name);
         cv::Mat matImg = cv::imread(ent->d_name, 0); // grayscale
         cv_image<unsigned char> cimg(matImg);
         assign_image(face_chips[index], cimg);
@@ -112,28 +132,37 @@ public:
     }
   }
 
-  int computeExpression(full_object_detection shape)
+  int computeExpression(full_object_detection shape, cv::Mat depthImg)
   {
-    //ROS_INFO("type: %s", typeid(shape.part(49).y()).name());
     int difference_ = abs(shape.part(49).y() - shape.part(55).y());
-    ROS_INFO("difference: %d", difference_);
-    if(difference_ > 15)
+    int midLipDist_ = abs(shape.part(62).y() - shape.part(66).y());
+    //ROS_INFO("mid lip difference: %d", midLipDist_);
+    unsigned short val = depthImg.at<unsigned short>(shape.part(30).y(), shape.part(30).x());
+    int depth_ = static_cast<int>(val); // depth in mm
+    if(depth_ == 0)
+    {
+      depth_ = 440; // closest range
+    }
+    ROS_INFO("depth: %d\n", depth_);
+    if(midLipDist_ > 20)
     {
       return 2;
     }
-    else if(difference_ > 10 && difference_ <= 15)
+    else if(midLipDist_ > 10 && midLipDist_ <= 20)
     {
       return 1;
     }
     return 0;
   }
 
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+  void imageCb(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::ImageConstPtr& depth_msg)
   {
     cv_bridge::CvImagePtr cv_ptr;
+    cv_bridge::CvImagePtr depth_ptr;
     try
     {
       cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+      depth_ptr = cv_bridge::toCvCopy(depth_msg, sensor_msgs::image_encodings::TYPE_16UC1);
     }
     catch (cv_bridge::Exception& e)
     {
@@ -144,6 +173,7 @@ public:
     try
     {
       cv_image<bgr_pixel> cimg(cv_ptr->image); // dlib wrapper for cv::Mat object
+      cv_image<unsigned short> dimg(depth_ptr->image);
       //pyramid_up(cimg); // upsample
       std::vector<rectangle> faces = detector_(cimg); // detected bounding boxes
       std::vector<full_object_detection> shapes; // pose of each face
@@ -152,8 +182,24 @@ public:
         // shape predictor generates 68 facial landmarks (iBUG 300-W scheme)
         full_object_detection shape = sp_(cimg, faces[j]);
         shapes.push_back(shape);
+//        dlib::draw_solid_circle(cimg, shape.part(48), 5, dlib::bgr_pixel(255,0,0)); //outer lip: left corner
+//        dlib::draw_solid_circle(cimg, shape.part(49), 5, dlib::bgr_pixel(255,0,0)); //outer lip: upper left
+//        dlib::draw_solid_circle(cimg, shape.part(50), 5, dlib::bgr_pixel(255,0,0)); //outer lip: left upper middle
+//        dlib::draw_solid_circle(cimg, shape.part(51), 5, dlib::bgr_pixel(255,0,0)); //outer lip: upper middle
+//        dlib::draw_solid_circle(cimg, shape.part(52), 5, dlib::bgr_pixel(255,0,0)); //outer lip: right upper middle
+//        dlib::draw_solid_circle(cimg, shape.part(53), 5, dlib::bgr_pixel(255,0,0)); //outer lip: upper right
+//        dlib::draw_solid_circle(cimg, shape.part(54), 5, dlib::bgr_pixel(255,0,0)); //outer lip: right corner
+//        dlib::draw_solid_circle(cimg, shape.part(55), 5, dlib::bgr_pixel(255,0,0)); //outer lip: lower right
+//        dlib::draw_solid_circle(cimg, shape.part(56), 5, dlib::bgr_pixel(255,0,0)); //outer lip: right lower middle
+//        dlib::draw_solid_circle(cimg, shape.part(57), 5, dlib::bgr_pixel(255,0,0)); //outer lip: lower middle
+//        dlib::draw_solid_circle(cimg, shape.part(58), 5, dlib::bgr_pixel(255,0,0)); //outer lip: left lower middle
+//        dlib::draw_solid_circle(cimg, shape.part(59), 5, dlib::bgr_pixel(255,0,0)); //outer lip: lower left
+//        dlib::draw_solid_circle(cimg, shape.part(60), 5, dlib::bgr_pixel(0,0,255)); //inner lip: left corner
+//        dlib::draw_solid_circle(cimg, shape.part(62), 5, dlib::bgr_pixel(0,0,255)); //inner lip: upper middle
+//        dlib::draw_solid_circle(cimg, shape.part(66), 5, dlib::bgr_pixel(0,0,255)); //inner lip: lower middle
+//        dlib::draw_solid_circle(cimg, shape.part(30), 5, dlib::bgr_pixel(0,0,255)); //nose tip
         std_msgs::Int8 expression_;
-        expression_.data = computeExpression(shape);
+        expression_.data = computeExpression(shape, depth_ptr->image);
         ROS_INFO("expression %d\n", expression_.data);
         expression_pub_.publish(expression_);
         //templates_.push_back(shape);
@@ -172,6 +218,7 @@ public:
       //cv_ptr->image = toMat(cimg); // converts dlib image back to cv::Mat
       win_.clear_overlay();
       win_.set_image(cimg);
+      //win_.set_image(dimg);
       // convert into contour overlays for visualizations
       win_.add_overlay(render_face_detections(shapes));
     }
@@ -182,8 +229,8 @@ public:
     }
 
     // Update GUI Window
-    //cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-    //cv::waitKey(3);
+    //cv::imshow(OPENCV_WINDOW, depth_ptr->image);
+    //cv::waitKey(1);
     
     // Output modified video stream
     //image_pub_.publish(cv_ptr->toImageMsg());
@@ -192,9 +239,9 @@ public:
 
 int main(int argc, char** argv)
 {
-  if (argc < 3)
+  if (argc < 2)
   {
-    ROS_ERROR("\nUsage: rosrun face_evaluator face_evaluator_node shape_predictor_68_face_landmarks.dat templatesFolder\n");
+    ROS_ERROR("\nUsage: rosrun face_evaluator face_evaluator_node shape_predictor_68_face_landmarks.dat\n");
     return 0;
   }
   deserialize(argv[1]) >> sp_;
